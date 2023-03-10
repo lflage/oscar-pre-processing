fa677054bc8a88eb8babda542cca0ccfef1631c7
Lucas Lage <lage.lucas@opendfki.de>
2023-03-10 18:02:08 +0100

    added generator func

--- test.py
+++ test.py
@@ -1,15 +1,31 @@
 import json,pprint
 pp = pprint.PrettyPrinter(indent=4)
-with open ("./oscar_eo/eo_meta.jsonl", 'r') as file:
-    lines = file.readlines()
-    print(len(lines))
-    print('\n')
-    for line in lines[:1]:
-        doc = json.loads(line)
-        print(doc.keys())
-        print("\n\n")
-        pp.pprint(doc['content'])
-        print("\n\n")
-        pp.pprint(doc['warc_headers'])
-        print("\n\n")
-        pp.pprint(doc['metadata'])
+
+
+def yield_dict(path):
+    with open(path, "r") as json_file:
+        for line in json_file:
+            yield json.loads(line)
+
+def get_n_lines(path):
+    with open(path) as f:
+        return len(f.readlines())
+
+
+path = "./el_oscar/el_meta_part_1.jsonl"
+
+dict_generator = yield_dict(path)
+print(get_n_lines(path))
+
+for doc in dict_generator:
+    
+    print(doc.keys())
+    # print("\n\n")
+    # pp.pprint(doc['warc_headers'])
+    # pp.pprint(doc['content'])
+    # print("\n\n")
+    # print("\n\n")
+    # pp.pprint(doc['metadata'])
+
+    pp.pprint(doc['metadata']['harmful_pp'])
+    break
