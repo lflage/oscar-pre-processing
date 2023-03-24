ea3fab36a35745321317a5ee79175c1af8b4923c
lage.lucas <lufo01@dfki.de>
2023-03-24 17:49:30 +0100

    Tested zstandard library

--- test.py
+++ test.py
@@ -1,13 +1,37 @@
-import json
-import pandas as pd
+import json, os
+import polars as pl
+import zstandard as zstd
 
 
-path =  "./pt_meta/pt_meta_part_64.jsonl"
+path =  "./dataset/"
+f_paths = []
 
-pddf = pd.read_json(path, lines=True)
+for dirpath, dirname, files in os.walk(path):
+    for file in files:
+        f_paths.append(os.path.join(dirpath,file))
 
-print(pddf.head(5))
+f_paths = [file for file in f_paths if file.endswith('.zst')]
 
-norm = pd.json_normalize(pddf, record_path=['content','metadata'], meta=[['content'],['metadata','tlsh'],['metadata','harmful_pp'],['metadata','quality_warnings']])
+for file in f_paths[:1]:
+    print(file)
+    output_path = file.replace('.zst', '')
+    dctx = zstd.ZstdDecompressor()
+    with open(file, 'rb') as ifh, open(output_path, 'wb') as ofh:
+        dctx.copy_stream(ifh, ofh)
+        
+    # with open(file, 'rb') as compressed:
+    #     dctx = zstd.ZstdDecompressor()
+    #     dctx.copy_stream(ifh, ofh)
+    #     decompressed = dctx.decompress(compressed.read())
+    
+decompressed = pl.read_ndjson(output_path)
 
-print(norm.head())
+print(decompressed.head())
+
+# pddf = pd.read_json(path, lines=True)
+
+# print(pddf.head(5))
+
+# norm = pd.json_normalize(pddf, record_path=['content','metadata'], meta=[['content'],['metadata','tlsh'],['metadata','harmful_pp'],['metadata','quality_warnings']])
+
+# print(norm.head())
