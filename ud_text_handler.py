1d91213e19cf6f5a0e2e3eba01242d2b71ca6bce
lage.lucas <lufo01@dfki.de>
2023-08-18 17:12:22 +0200

    UD Text handler now works

--- ud_text_handler.py
+++ ud_text_handler.py
@@ -1,10 +1,10 @@
 import os, shutil
 from pprint import pprint
-from utils import lang_dict
+from utils.Utils import lang_dict, ud_concat
 from tqdm import tqdm
 
 
-print('in file')
+print('file')
 # Ordem:
 # TODO:
 #       - turn into function
@@ -16,9 +16,9 @@ languages = [('English','en'),('Swedish','sv'), ('Russian','ru'),('French','fr')
 
 init_dir = os.getcwd()
 # Output path
-ud_o_path = './new_/'
+ud_o_path = '/netscratch/fonseca/oscar-pre-processing/data/processed/filter_ud/'
 # input path
-ud_i_path = '/netscratch/fonseca/ud/ud-treebanks-v2.11/'
+ud_i_path = '/netscratch/fonseca/oscar-pre-processing/data/raw/ud/ud-treebanks-v2.11/'
 
 if not os.path.exists(ud_o_path):
     os.mkdir(ud_o_path)
@@ -26,75 +26,27 @@ if not os.path.exists(ud_o_path):
 f_paths = []
 dir_paths = []
 
-for dirpath, dirname, files in os.walk(ud_i_path):
-    for file in files:
-        if ".txt" in file:
-            path = os.path.join(dirpath,file)
-            n_path = "/".join(path.split('/')[-2:])
+# for dirpath, dirname, files in os.walk(ud_i_path):
+#     for file in files:
+#         if ".txt" in file:
+#             path = os.path.join(dirpath,file)
+#             n_path = "/".join(path.split('/')[-2:])
 
-            try:
-                for lg, lang in lang_dict.items():
-                    if lang in n_path:
-                        folder = os.path.dirname(ud_o_path+n_path)
-                        os.makedirs(folder,exist_ok=True)
-                        shutil.copy(ud_i_path+n_path, ud_o_path+n_path)
-                        dir_paths.extend([folder])
-                    else:
-                        continue
-            except FileNotFoundError as e:
-                print(path)
-                print(e)
-        else:
-            continue
+#             try:
+#                 for lg, lang in lang_dict.items():
+#                     if lang in n_path:
+#                         folder = os.path.dirname(ud_o_path+n_path)
+#                         os.makedirs(folder,exist_ok=True)
+#                         shutil.copy(ud_i_path+n_path, ud_o_path+n_path)
+#                         dir_paths.extend([folder])
+#                     else:
+#                         continue
+#             except FileNotFoundError as e:
+#                 print(path)
+#                 print(e)
+#         else:
+#             continue
 
-
-
-print("-----------------------")
-if dir_paths:
-    print('exists')
-print(dir_paths[:10])
-for folder in dir_paths:
-    text = ""
-    print("checking folder:", folder)
-    for root,dirname, files in os.walk(folder):
-        files = [file for file in files if "README" not in file]
-        files = [file for file in files if "LICENSE" not in file]
-        files = [file for file in files if ".py" not in file]
-        print('passed file filtering')
-        for file in files:
-            read_path = folder+'/'+file
-            print("reading:", read_path)
-            with open(read_path, "r") as dtt:
-                text += dtt.read()
-    concat_path = folder+"/"+ folder.split('/')[-1]+ "-concat.txt"
-    print("concat_path:",concat_path)
-    with open(concat_path, "a") as concat:
-
-        concat.write(text)
-
-
-
-# dirs = []
-# for dirpath, dirname, files in os.walk(ud_o_path):
-#     if dirname:
-#         dirs.extend(dirname)
-
-# for dir in dirs:
-#     for 
-    # for file in files:
-    #     n_path = os.path.join(dirpath,file)
-
-
-# pprint(f_paths)
-# for dirname in dir_paths:
-#     text = ""
-#     for file_path in f_paths:
-#         if dirname in file_path:
-#             with open(file_path,'r') as file:
-#                 text += file.read()
-#     print(dirname)
-#     with open( dirname +'-full.txt','w') as c_file:
-#         c_file.write(text)
-#     print(dirname)
-
+for dr in os.listdir(ud_o_path):
+    ud_concat(os.path.join(ud_o_path,dr))
 
